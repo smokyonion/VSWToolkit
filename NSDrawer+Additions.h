804539ffdcff7ae5a49c2c82763156c71dd347e2
smokyonion <smokyonion@gmail.com>
2010-08-25 11:04:58 +0800

    add NSDrawer+Additions

---
+++ NSDrawer+Additions.h
@@ -0,0 +1,16 @@
+//
+//  NSDrawer+Additions.h
+//  CocoaCategories
+//
+//  Created by vincent on 8/25/10.
+//  Copyright 2010 Ortery Technologies, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSDrawer (Additions)
+
+- (IBAction)toggleByFixedEdge:(id)sender;
+
+@end
