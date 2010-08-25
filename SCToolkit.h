3010ecfd2d9f988eae5b11846d8f9eb3f41b236f
smokyonion <smokyonion@gmail.com>
2010-08-25 14:37:27 +0800

    add SCToolkit target

---
+++ SCToolkit.h
@@ -0,0 +1,14 @@
+//
+//  SCToolkit.h
+//  CocoaCategories
+//
+//  Created by vincent on 8/25/10.
+//  Copyright 2010 Ortery Technologies, Inc. All rights reserved.
+//
+
+
+#import "NSImage+Additions.h"
+#import "NSObject+Additions.h"
+#import "NSDrawer+Additions.h"
+#import "NSString+Additions.h"
+#import "NSUserDefaults+Additions.h"
