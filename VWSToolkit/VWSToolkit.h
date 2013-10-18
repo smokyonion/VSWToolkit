e4bdc616067d32fddf0d86875f493ff204daf763
Vincent Wang <vwang@masimo.com>
2013-10-18 09:33:00 -0700

    project reborn

---
+++ VWSToolkit/VWSToolkit.h
@@ -0,0 +1,13 @@
+//
+//  VWSToolkit.h
+//  VWSToolkit
+//
+//  Created by Vincent Wang on 10/18/13.
+//  Copyright (c) 2013 Vincent Wayne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VWSToolkit : NSObject
+
+@end
---
+++ VWSToolkit/VWSToolkit.m
@@ -0,0 +1,13 @@
+//
+//  VWSToolkit.m
+//  VWSToolkit
+//
+//  Created by Vincent Wang on 10/18/13.
+//  Copyright (c) 2013 Vincent Wayne. All rights reserved.
+//
+
+#import "VWSToolkit.h"
+
+@implementation VWSToolkit
+
+@end
